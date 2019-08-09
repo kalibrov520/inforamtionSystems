c4328785a83acacf153eb99cd682e11b18dd63b9
Ilya Kalibrov <Ilya.Kalibrov@emergn.com>
2019-08-09 17:46:45 +0300

    FtpWatcher initial implementation

---
+++ FtpWatcherService/FtpWatcher/FtpWatcher/Handlers/FtpWatcherHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Camunda.Worker;
+
+namespace FtpWatcher.Handlers
+{
+    [HandlerTopics("FtpWatcher")]
+    public class FtpWatcherHandler : ExternalTaskHandler
+    {
+        public override Task<IExecutionResult> Process(ExternalTask externalTask)
+        {
+            return Task.FromResult<IExecutionResult>(new CompleteResult(new Dictionary<string, Variable>()));
+        }
+    }
+}
