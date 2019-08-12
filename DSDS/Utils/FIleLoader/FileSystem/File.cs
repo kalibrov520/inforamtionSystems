e82b42466884970a78fbeba54d76713aa64a32f3
Ilya Kalibrov <Ilya.Kalibrov@emergn.com>
2019-08-12 15:51:26 +0300

    refactoring

--- DSDS/Utils/FIleLoader/FileSystem/File.cs
+++ DSDS/Utils/FIleLoader/FileSystem/File.cs
@@ -15,7 +15,7 @@ namespace FileLoader.FileSystem
 
         public bool Equals(File other)
         {
-            if (ReferenceEquals(null, other)) return false;
+            if (other is null) return false;
             if (ReferenceEquals(this, other)) return true;
             return Size.Equals(other.Size) && string.Equals(FullPath, other.FullPath) && string.Equals(Name, other.Name) && LastModified.Equals(other.LastModified);
         }
@@ -24,8 +24,7 @@ namespace FileLoader.FileSystem
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((File) obj);
+            return obj.GetType() == this.GetType() && Equals((File) obj);
         }
     }
 }
