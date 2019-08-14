eba3da093b16240415d4d86299bbbbba6d24a050
Ilya Kalibrov <Ilya.Kalibrov@emergn.com>
2019-08-14 11:42:09 +0300

    initial create

---
+++ DSDS/ScriptExecutorService/Handlers/ScriptExecutorHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Camunda.Worker;
+
+namespace ScriptExecutorService.Handlers
+{
+    [HandlerTopics("ScriptExecutor")]
+    public class ScriptExecutorHandler : ExternalTaskHandler
+    {
+        public override Task<IExecutionResult> Process(ExternalTask externalTask)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
