fb218f678e2caab5f0ecf2f343b1c8a33e6c6d4e
Ilya Kalibrov <Ilya.Kalibrov@emergn.com>
2019-09-02 13:39:44 +0300

    initial create

---
+++ DSDS/FileLoaderService/Handlers/FileLoaderHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Camunda.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FileLoaderService.Handlers
+{
+    [HandlerTopics("FileLoader")]
+    public class FileLoaderHandler : ExternalTaskHandler
+    {
+        private readonly ILogger<FileLoaderHandler> _logger;
+
+        public FileLoaderHandler(ILogger<FileLoaderHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<IExecutionResult> Process(ExternalTask externalTask)
+        {
+            try
+            {
+                var a = JsonConvert.DeserializeObject<List<FileLoader.FileSystem.File>>(externalTask.Variables["newFiles"].AsString());
+
+                foreach (var (variableKey, variableValue) in externalTask.Variables.Where(variable => variable.Value.Type == VariableType.Bytes))
+                {
+                    using (var fileWriter = new StreamWriter(".\\" + variableKey + ".bat"))
+                    {
+                        fileWriter.Write(System.Text.Encoding.Default.GetString(variableValue.AsBytes()));
+                        _logger.LogInformation("Copy {fileName}.bat locally...", variableKey);
+                    }
+
+                    System.Diagnostics.Process.Start(variableKey + ".bat");
+                    _logger.LogInformation("Starting file {fileName}.bat...", variableKey);
+                }
+
+                return Task.FromResult<IExecutionResult>(new CompleteResult(new Dictionary<string, Variable>()
+                {
+                    ["formattedFile"] = Variable.Bytes(File.ReadAllBytes(".\\doc.xlsx"))
+                }));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception occured, while executing ScriptExecutorHandler");
+                return Task.FromException<IExecutionResult>(ex);
+            }
+        }
+    }
+}
---
+++ DSDS/FileLoaderService/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Camunda.Worker;
+using Camunda.Worker.Extensions;
+using FileLoaderService.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace FileLoaderService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCamundaWorker(options =>
+            {
+                options.WorkerId = "fileLoader";
+                options.WorkerCount = 1;
+                options.BaseUri = new Uri("http://localhost:8080/engine-rest");
+            }).AddHandler<FileLoaderHandler>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+        }
+    }
+}
