a4c57acec526e3d5325a67868b76aabbd22e037b
Sergey Pereyaslov <sergey.pereyaslov@emergn.com>
2019-09-19 14:56:02 +0300

    set CQRS header for reqeust fromnm UI

--- client/src/app/services/dataFeed.service.ts
+++ client/src/app/services/dataFeed.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.dev';
 import { Http, Response } from '@angular/http';
+import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { IDataFeed, DataFeed, DataFeedDetails } from '../models/dataFeed';
+import { from } from 'rxjs';
 
 const API_URL = environment.apiUrl;
 
@@ -13,11 +15,19 @@ const API_URL = environment.apiUrl;
   providedIn: 'root'
 })
 export class DataFeedService {
-  constructor(private http: Http) {}
+  httpOptions;  
+
+  constructor(private http: Http) {
+    this.httpOptions = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'*'
+      })
+    };
+  }
 
   public getAllDataFeedsInfo(): Observable<IDataFeed[]> {
     return this.http
-      .get(API_URL + "/api/datatransformation").pipe(map(response => {
+      .get(API_URL + "/api/datatransformation", this.httpOptions).pipe(map(response => {
            let dataFeeds = response.json();
            return dataFeeds.map(dataFeed => new DataFeed(dataFeed));
          })).catch(this.handleError);
@@ -25,14 +35,14 @@ export class DataFeedService {
 
   public getDataFeedDetails(deploymentId: string): Observable<DataFeedDetails[]> {
     return this.http
-      .get(API_URL + "/api/datatransformation/" + deploymentId).pipe(map(response => {
+      .get(API_URL + "/api/datatransformation/" + deploymentId, this.httpOptions).pipe(map(response => {
            let dataFeeds = response.json();
            return dataFeeds.map(dataFeed => new DataFeedDetails(dataFeed));
          })).catch(this.handleError);
   }
 
   public getDataFeedFailesInfo(runId: string): Observable<string[]> {
-    return this.http.get(API_URL + "/api/datatransformation/datafeedfailes/" + runId).pipe(map(response => {
+    return this.http.get(API_URL + "/api/datatransformation/datafeedfailes/" + runId, this.httpOptions).pipe(map(response => {
       let data = response.json();
       return data;
     })).catch(this.handleError);
