0ecfec20d2958d02b62f6ec45be8750daf228903
Dmitriy Volkov <Dmitriy.Volkov@emergn.com>
2019-09-23 18:17:38 +0300

    @DSDS-41 Test user can view the invalid records for specific Process Run

--- DSDS E2E Tests/stepdefinitions/inboundPageSteps.ts
+++ DSDS E2E Tests/stepdefinitions/inboundPageSteps.ts
@@ -182,9 +182,25 @@ Given(/^A 'Feed File' is missing$/, async () => {
 
 When(/^Operator clicks on 'Inbound Data Source' item in the list$/, async () => {
 
-    let row = element.all(by.xpath("//mercer-table/table/tbody/tr"));
-    let links = row.all(by.tagName('a'));
-    let first = await links.first().getAttribute("href");
+    let rows = element.all(by.xpath("//mercer-table/table/tbody/tr"));
+    let rowsFailed = await rows.all(by.className("mos-t-circle--alert")).count();
+
+    let first;
+    let links ; 
+    if (rowsFailed > 0) {
+        let rowsArray = await rows.getText();
+        for (let i = 0; i < rowsArray.length; i++) {
+            let words = rowsArray[i].split(" ");
+            if (words[words.length - 1] !== "0") {
+                links = rows.get(i);
+                first = await links.element(by.tagName('a')).getAttribute("href");
+                break;
+            }
+        }
+    } else {
+        links = rows.all(by.tagName('a'));
+        first = await links.first().getAttribute("href");
+    }
 
     await browser.get(first);
 });
@@ -196,18 +212,57 @@ Then(/^Operator lands on  'Feed History' page$/, async () => {
 Then(/^Operator can see 'Process Run' log with following atributes: Status, Date, Success Rows, Failed Rows$/, async () => {
     let row = element.all(by.xpath("//mercer-table/table/thead/tr"));
     let text = await row.first().getText();
-    
+
     expect(text).to.match(/Status Date Success Rows Failed Rows File/);
 });
 
 //@DSDS-41 Test user can view the invalid records for specific Process Run
 
 Given(/^A process is completed with 'Failed' status$/, async () => {
-    throw new PendingException();
+    let rows = element.all(by.xpath("//mercer-table/table/tbody/tr"));
+    let rowsFailed = rows.all(by.className("mos-t-circle--alert"));
+
+    expect(rowsFailed.count()).to.eventually.above(0);
 });
 
-When(/^Operator clicks on 'Feed History' page$/, async () => {
-    throw new PendingException();
+When(/^Operator clicks on  the hyperlink with # of failed rows$/, async () => {
+    let rows = element.all(by.xpath("//mercer-table/table/tbody/tr"));
+
+
+    
+    let links;
+    let first;
+    let flag = false;
+
+
+    for (let i = 0; i < await rows.count(); i++) {
+        links = rows.get(i).all(by.tagName('a'));
+
+        for (let j = 0; j < await links.count(); j++) {
+            let link = links.get(j);
+            first = await link.getAttribute("href");
+            
+        
+            if (first !== "javascript:;" && first !== "#") {
+                console.log(first);
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            break;
+        }
+    }
+
+    await browser.get(first);
+});
+
+Then(/^Operator can see 'Invalid Records' log$/, async () => {
+    let row = element.all(by.xpath("//mercer-table/table/thead/tr"));
+    let text = await row.first().getText();
+
+    expect(text).to.match(/ISN FundID FundName MarketValue Shares NAV MarketDate ValidationDate/);
 });
 
 //File Management: Trigger feed process
